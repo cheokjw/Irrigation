3afe4800a98182dbda7d840ac69caef665f60dfd
cheokjw <cheok1121@gmail.com>
2023-09-02 01:02:55 +0800

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -2,7 +2,9 @@
 import streamlit as st
 from datetime import datetime
 import os
+import json
 from google.cloud import firestore
+from google.oauth2 import service_account
 
 
 # DATABASE ------------------------------------------------
@@ -13,4 +15,3 @@ db = firestore.Client.from_service_account_json('firestore_key.json')
 collections = [collection.id for collection in db.collections()]
 for collection_name in collections:
     print(collection_name)
-    
