0361fbf1220eb5a6ed5d07bcbac39613346575f2
cheokjw <cheok1121@gmail.com>
2023-09-01 22:12:32 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,24 @@
 import streamlit as st
 from google.cloud import firestore
 
-# Connect to firestore database by using JSON account key
-db = firestore.Client.from_service_account_json("firestore_key.json")
 
-ref = db.collection("plant").document("plantData")
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
 
-# Obtain data from the collection
-doc = ref.get()
+filename = file_selector()
+st.write('You selected `%s`' % filename)
 
-# Display obtained data
-st.header("Smart Irrigation System")
-st.write(f"The id is {doc.id}")
-st.write(f"The contents are {doc.to_dict()}")
+# # Connect to firestore database by using JSON account key
+# db = firestore.Client.from_service_account_json("firestore_key.json")
+
+# ref = db.collection("plant").document("plantData")
+
+# # Obtain data from the collection
+# doc = ref.get()
+
+# # Display obtained data
+# st.header("Smart Irrigation System")
+# st.write(f"The id is {doc.id}")
+# st.write(f"The contents are {doc.to_dict()}")
