74404712ca7786accec9e5b849a6f804e31107eb
cheokjw <cheok1121@gmail.com>
2023-09-01 22:45:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,41 @@
+# Import Necessary Libraries
+from datetime import datetime
 import streamlit as st
 import os
 from google.cloud import firestore
 
+curr = datime.now().strftime('%d/%m/%Y %H:%M:%S')
+# DATABASE ------------------------------------------------
 # Connect to firestore database by using JSON account key
-db = firestore.Client.from_service_account_json("firestore_key.json")
+db = firestore.Client.from_service_account_json('firestore_key.json')
 
-ref = db.collection("plant").document("plantData")
 
-# Obtain data from the collection
-doc = ref.get()
+# Write Sample data into firestore
+doc_ref = db.collection('plant').document(curr)
+doc_ref.set({
+    'distance': 12,
+    'humidity': 30,
+    'lightIntensity': 50,
+    'moisture': 20,
+    'pH': 5,
+    'temperature': 28
+})
 
+
+# Read all posts (Change to 'user' in the future)
+posts_ref = db.collection('plant')
+# ----------------------------------------------------------
+
+
+
+# STREAMLIT ------------------------------------------------
 # Display obtained data
-st.header("Smart Irrigation System")
-st.write(f"The id is {doc.id}")
-st.write(f"The contents are {doc.to_dict()}")
+st.header('Smart Irrigation System')
+
+for doc in post_ref.stream():
+    st.write(f'The id is: {doc.id}')
+    st.write(f'The contents are: ', doc.to_dict())
+    
+# ----------------------------------------------------------
 
 
