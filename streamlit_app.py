04bf92b8e409e53381d611ff29b4fb5d44b44315
cheokjw <cheok1121@gmail.com>
2023-09-01 21:57:28 +0800

    read_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from google.cloud import firestore
 
+# Connect to firestore database by using JSON account key
+db = firestore.Client.from_service_account_json("firestore-key.json")
 
+ref = db.collection("plant").document("plantData")
 
+# Obtain data from the collection
+doc = ref.get()
+
+# Display obtained data
 st.header("Smart Irrigation System")
+st.write(f"The id is {doc.id}")
+st.write(f"The contents are {doc.to_dict()}")
