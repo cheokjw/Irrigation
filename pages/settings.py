45d69c8127aff75086c3e90c94f153eb7ee24ef9
cheokjw <cheok1121@gmail.com>
2023-09-03 17:01:30 +0800

    Update settings.py

--- pages/settings.py
+++ pages/settings.py
@@ -54,25 +54,25 @@ col1, col2, col3, col4 = st.columns(4)
 # First Radio button has been deselected 
 with col1:
     if user_choice == 1:
-        st.radio(label='Plant 1', options=['One', 'Plant 1'], index=2, label_visibility='collapsed')
+        st.radio(label='Plant 1', options=['One', 'Plant 1'], index=1, label_visibility='collapsed')
     else: 
         st.radio(label='Plant 1', options=['One', 'Plant 1'], label_visibility='collapsed')
 
 
 with col2:
     if user_choice == 2:
-        st.radio(label='Plant 2', options=['One', 'Plant 2'], index=2, label_visibility='collapsed')
+        st.radio(label='Plant 2', options=['One', 'Plant 2'], index=1, label_visibility='collapsed')
     else: 
         st.radio(label='Plant 2', options=['One', 'Plant 2'], label_visibility='collapsed')
 
 with col3:
     if user_choice == 3:
-        st.radio(label='Plant 3', options=['One', 'Plant 3'],index=2, label_visibility='collapsed')
+        st.radio(label='Plant 3', options=['One', 'Plant 3'],index=1, label_visibility='collapsed')
     else: 
         st.radio(label='Plant 3', options=['One', 'Plant 3'], label_visibility='collapsed')
 
 with col4:
     if user_choice == 3:
-        st.radio(label='Customize', options=['One', 'Customize'], index=2, label_visibility='collapsed')
+        st.radio(label='Customize', options=['One', 'Customize'], index=1, label_visibility='collapsed')
     else: 
         st.radio(label='Customize', options=['One', 'Customize'], label_visibility='collapsed')
