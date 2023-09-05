5eacb118779fe4dd653256773dfb85f0742d6ad4
cheokjw <cheok1121@gmail.com>
2023-09-05 09:52:50 +0800

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -74,16 +74,10 @@ for doc in post_ref.stream():
     st.dataframe(df)
     data = doc.to_dict()
     st.write(f'pH :{data["humidity"]}, {type(data["humidity"])}')
-    temp_df = pd.DataFrame({
-        'datetime': [doc.id],
-        'distance': [data["distance"]],
-        'humidity': [data["humidity"]],
-        'lightIntensity': [data["lightIntensity"]],
-        'moisture': [data["moisture"]],
-        'pH': [data["pH"]],
-        'temperature': [data["temperature"]]
-    })
-    df.append(temp_df)
+    row = pd.Series(
+        [doc.id, data["distance"], data["humidity"], data["lightIntensity"], data["moisture"], data["pH"], data["temperature"]], index=df.columns
+    )
+    df.append(row)
 
 st.dataframe(df)
 
