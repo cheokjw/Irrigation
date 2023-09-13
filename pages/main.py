22a95e8c7ddb8c568bc31814d4c6979ee900a250
cheokjw <cheok1121@gmail.com>
2023-09-13 11:54:49 +0800

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -79,10 +79,9 @@ df = pd.DataFrame({'datetime': ['01-09-2023 15:23:23'],
                     'pH': [7], 
                     'temperature':[28]})
 
-
+st.write(mac_ref)
 for doc in mac_ref.stream():
     data = doc.to_dict()
-    st.write(data)
     temp_df = pd.DataFrame({
         'datetime': [doc.id],
         'distance': [data["distance"]],
