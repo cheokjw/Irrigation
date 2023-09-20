291447404ef5efeb442dd582c92b27a51cb42901
cheokjw <cheok1121@gmail.com>
2023-09-20 13:16:27 +0800

    mqtt

--- pages/main.py
+++ pages/main.py
@@ -40,21 +40,31 @@ post_ref = db.collection(current_user_ref['user'])
 
 
 # MQTT -----------------------------------------------------
-def on_message(client, userdata, message):
-    st.write(f"Received Message on Topic {message.topic}: {message.payload.decode()}")
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    st.write("Connected with result code "+str(rc))
 
-# MQTT Configuration
-mqtt_broker = 'mqtt.eclipseprojects.io'
-mqtt_port = 1883 
-mqtt_topic = '$SYS/#'
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe('paho/IOTtest')
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    st.write(msg.topic+" "+str(msg.payload))
 
 # Connect to MQTT client
 client = mqtt.Client()
+client.on_connect = on_connect
 client.on_message = on_message
-client.connect(mqtt_broker, mqtt_port)
-client.subscribe(mqtt_topic)
-client.loop_start()
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+client.subscribe('paho/IOTtest')
 
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop()
 # ----------------------------------------------------------
 
 
